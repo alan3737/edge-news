facd64ed1a090e9640611a7a5e0b1015be96681e
AZ <alanzheng2323@gmail.com>
2025-07-13 01:40:56 -0400

    Initial setup of Angular frontend and Spring Boot backend

---
+++ backend/edge-news/src/main/java/com/econ/edge_news/EdgeNewsApplication.java
@@ -0,0 +1,13 @@
+package com.econ.edge_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeNewsApplication.class, args);
+	}
+
+}
---
+++ backend/edge-news/src/test/java/com/econ/edge_news/EdgeNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econ.edge_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
