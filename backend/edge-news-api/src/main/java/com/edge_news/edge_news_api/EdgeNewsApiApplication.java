3544b5e558e1d3b5f21a3189a9aedc1088635aa8
EdisonZheng2323 <edisonzheng3737@gmail.com>
2026-08-15 18:46:35 -0400

    Reset spring boot backend to starter code

---
+++ backend/edge-news-api/src/main/java/com/edge_news/edge_news_api/EdgeNewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.edge_news.edge_news_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeNewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeNewsApiApplication.class, args);
+	}
+
+}
---
+++ backend/edge-news-api/src/test/java/com/edge_news/edge_news_api/EdgeNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edge_news.edge_news_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
